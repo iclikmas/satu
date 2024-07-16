9c82958d7eb58175c4a169a45b972afc97007dd6
iclikmas <alayawrada@gmail.com>
2024-07-16 20:10:25 +0700

    Update add-ip-bot.sh

--- bot/add-ip-bot.sh
+++ bot/add-ip-bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # // PROJECT XDXL STORE
-url_izin="https://raw.githubusercontent.com/SatanFusionOfficial/permission/main/ip"
+url_izin="https://raw.githubusercontent.com/iclikmas/izin_satu/main/ip"
 IP=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
@@ -17,16 +17,16 @@ checking_sc() {
     echo -e "   \033[0;33mYour VPS${NC} $IP \033[0;33mHas been Banned${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mWhatsapp${NC} wa.me/6282240074362"
+    echo -e "      \033[0;36mWhatsapp${NC} wa.me/6281917110387"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit
   fi
 }
 checking_sc
-# Token SatanFusionOfficial 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
-TOKEN="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"
+# Token iclikmas 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
+TOKEN="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"
 today=`date -d "0 days" +"%Y-%m-%d"`
-git clone https://github.com/SatanFusionOfficial/permission.git /root/ipvps/ &> /dev/null
+git clone https://github.com/iclik/izin_satu.git /root/ipvps/ &> /dev/null
 clear
 echo -e ""
 read -p "Input IP Address : " ip
@@ -42,14 +42,14 @@ read -p " Masukan waktu expired : " -e exp
 exp2=`date -d "${exp} days" +"%Y-%m-%d"`
 echo "### ${name} ${exp2} ${ip}" >> /root/ipvps/ip
 cd /root/ipvps
-git config --global user.email "sfvpnteam752@gmail.com" &> /dev/null
-git config --global user.name "SatanFusionOfficial" &> /dev/null
+git config --global user.email "alayawrada@gmail.com" &> /dev/null
+git config --global user.name "iclikmas" &> /dev/null
 rm -rf .git &> /dev/null
 git init &> /dev/null
 git add . &> /dev/null
 git commit -m register &> /dev/null
 git branch -M main &> /dev/null
-git remote add origin https://github.com/SatanFusionOfficial/permission
-git push -f https://${TOKEN}@github.com/SatanFusionOfficial/permission.git &> /dev/null
+git remote add origin https://github.com/iclikmas/izin_satu
+git push -f https://${TOKEN}@github.com/iclikmas/izin_satu.git &> /dev/null
 rm -rf /root/ipvps
 clear
